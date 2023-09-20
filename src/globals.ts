06a6a3277fd7567c4b92a2694da8f072b03df30d
Martin Brablík <martin.brablik02@upol.cz>
2023-09-20 14:02:25 +0200

    korekce appUrl

--- src/globals.ts
+++ src/globals.ts
@@ -1,7 +1,7 @@
 import { reactive } from 'vue';
 
 export const globals = reactive({
-  //appUrl: 'http://localhost',
-  appUrl: `https://www.izus.${navigator.language == 'sk' ? 'sk' : 'cz'}/`,
+  appUrl: 'http://localhost',
+  //appUrl: `https://www.izus.${navigator.language == 'sk' ? 'sk' : 'cz'}/`,
   logoutQuery: '?logout=true',
 });
