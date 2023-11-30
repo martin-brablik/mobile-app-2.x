8bf6d3400065c8b3ad822eacfc19b328e91124d2
Martin Brablík <martin.brablik02@upol.cz>
2023-11-30 13:00:54 +0100

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,7 +7,7 @@ const config: CapacitorConfig = {
   appendUserAgent: '; iZUS-mobile-app/2.1.1',
   server: {
     androidScheme: 'http',
-    url: 'https://izustest.8u.cz/'
+    url: 'https://m.izus.cz/'
   }
 };
 
